2ea885eba3aab65196aa32325af00c25d7617ed7
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-04-20 16:09:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+      "My name is Stephen."
+    end
+  
+    get '/hometown' do 
+      "My hometown is Ocean Township, NJ"
+    end
+  
+    get '/favorite-song' do
+      "My favorite song is 'Sky's The Limit' by The Notorious B.I.G."
+    end
+
 end
